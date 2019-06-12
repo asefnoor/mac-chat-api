f53d2af1fea031e73af7abdceb36e990ea6b620e
Asif Noor <asef.noor@gmail.com>
2019-06-12 18:21:55 -0400

    Update index.js
    updated configuration

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb+srv://asif:Asif2019@cluster0-3q15u.mongodb.net/test?retryWrites=true&w=majority",
   "bodyLimit": "100kb"
 }
